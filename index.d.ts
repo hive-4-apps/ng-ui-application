23bea547092c116ba35a14aec7e2539d02569e6a
johanmaia <jomaia59@gmail.com>
2016-12-19 14:41:19 +0100

    Good beggining
    First step to get a right library

---
+++ index.d.ts
@@ -0,0 +1,3 @@
+export * from './src/application/application.component';
+export declare class HiveModule {
+}
---
+++ src/application/application.component.d.ts
@@ -0,0 +1,5 @@
+export declare class UIApplicationComponent {
+    logo: String;
+    name: String;
+    constructor();
+}
